2c891f352a7d2da8c97db44203640246d1e3df47
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-14 06:55:11 -0400

    show proper map

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,19 @@
-var indexCtrl = PennAppsXMIT.controller('indexCtrl', ['$window', '$scope',
-  function($window, $scope) {
+var indexCtrl = PennAppsXMIT.controller('indexCtrl', ['$window', '$scope', '$sce',
+  function($window, $scope, $sce) {
     $scope.events = $window.events;
     $scope.current = 0;
     $scope.formatTime = function(dt) {
       return moment(dt).format('llll');
     };
+    $scope.toggle = function(i) {
+      if($scope.events[i].location) {
+        $scope.current = i;
+      }
+    };
     $scope.mapSrc = function() {
       var location = encodeURIComponent($scope.events[$scope.current].location);
-      return "https://www.google.com/maps/embed/v1/place?key=AIzaSyBvdX9-poijqyQEnkkbfjZH0y51f7xO764&q=" + location;
+      var url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBvdX9-poijqyQEnkkbfjZH0y51f7xO764&q=" + location;
+      return $sce.trustAsResourceUrl(url);
     };
   }
 ]);
