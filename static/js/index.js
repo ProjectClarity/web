a489c6f0af36f349ce58694d8f5e1293024cec34
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-14 06:51:14 -0400

    add google map

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 var indexCtrl = PennAppsXMIT.controller('indexCtrl', ['$window', '$scope',
   function($window, $scope) {
     $scope.events = $window.events;
+    $scope.current = 0;
     $scope.formatTime = function(dt) {
       return moment(dt).format('llll');
+    };
+    $scope.mapSrc = function() {
+      var location = encodeURIComponent($scope.events[$scope.current].location);
+      return "https://www.google.com/maps/embed/v1/place?key=AIzaSyBvdX9-poijqyQEnkkbfjZH0y51f7xO764&q=" + location;
     };
   }
 ]);
